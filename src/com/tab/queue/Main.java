1f5811b87936b5b3f2b0182971c9ed71e4ffc2f0
olwj <al.wajgert@gmail.com>
2017-03-09 20:44:49 +0100

    add files

---
+++ src/com/tab/queue/Main.java
@@ -0,0 +1,16 @@
+package com.tab.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        QueueElements queue = new QueueElements();
+
+        queue.add("test");
+        queue.add("test2");
+        queue.add("cos");
+
+        queue.show();
+
+    }
+}
